37fba4026c903ac893a69e394e6ea088d47558e9
FaithOgendi <faithbrianna01@gmail.com>
2024-01-17 13:34:43 +0300

    html elements

--- index.js
+++ index.js
@@ -2,12 +2,17 @@
 
 
 // Event listerners for displaying the job details
-document.addEventListener('DOMContentLoaded',()=>{
+//document.addEventListener('DOMContentLoaded',()=>{
 
-})
+//})
 // function to display details of job listing
 //[company image, job title,location, No of applicants]
 //view job roles- button- click
 //Apply Now- button - click
+//Submit Application-click
+function toggleForm(applicationForm) {
+    var formContainer = document.getElementById("applicationForm");
+    formContainer.style.display = formContainer.style.display === "none" ? "block" : "none";
+}
 
 //alert box - alerts the user upon successful application
